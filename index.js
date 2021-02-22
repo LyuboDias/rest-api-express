1fc85a584a2c0b4345fd60518dd902f120eec1e0
Lyubomir Dias <lyubomirdias@gmail.com>
2021-02-22 10:41:57 +0000

    added comments

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+//  we use middleware with .use(express.json()) to parse(convirt the body to) JSON before we handle the request
 app.use(express.json())
 
+//  fire up server with app.listen and add second arg function 'when is ready console.log'
 app.listen(
   PORT,
   () => console.log(`its live at http://localhost:${PORT}`)
   )
 
+//  adding end point, handle the request with call back function that takes req and res
 app.get('/car', (req, res) => {
   res.status(200).send({
     car: 'BMW X5',
@@ -16,7 +19,7 @@ app.get('/car', (req, res) => {
   })
 });
 
-
+//  :id is dynamic URI param
 app.post('/car/:id', (req, res) => {
 
   const { id } = req.params;
